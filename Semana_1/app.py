28bc307341856c6ac40343e6f5e26e8b9ea870f5
michellesarahi14 <michellesarahi14@gmail.com>
2026-09-24 10:37:18 -0600

    faltaba indentación en el if

--- Semana_1/app.py
+++ Semana_1/app.py
@@ -10,6 +10,6 @@ variable_y = np.array([15,25,35,45,55])
 modelo_lr = LinearRegression()
 modelo_lr.fit(variable_x,variable_y)
 if st.button("Predecir"):
-resultado = modelo_lr.predict([[gasto]])
-st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:
-${resultado[0]}")
+    resultado = modelo_lr.predict([[gasto]])
+    st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:
+    ${resultado[0]}")
